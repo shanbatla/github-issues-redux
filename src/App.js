35c0b3e73a05c640b7cb932eb0e7af7164acf906
Shan Batla <shanbatla@Shans-MacBook-Pro-2.local>
2016-08-28 14:42:30 -0700

    Add Gridle; incremental style changes and code cleanup.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-
-          <h2>Welcome to React</h2>
+          <h2>Github Issue Tracker</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
